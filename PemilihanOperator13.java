75028e4e63dcfc51178f899272867e6a74d7be57
Nurwaely12 <nurwaely@gmail.com>
2024-09-24 18:10:51 +0700

    PemilihanOperator13

---
+++ PemilihanOperator13.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PemilihanOperator13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = sc.next().charAt(0);
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            if (angka2 != 0) {
+                hasil = angka1 / angka2;
+            } else {
+                System.out.println("Kesalahan: Pembagian dengan nol tidak diperbolehkan.");
+                return;
+            }
+        } else {
+            System.out.println("Operator yang dipilih tidak sesuai");
+            return;
+        }
+
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+    }
+}
+
+    
+
