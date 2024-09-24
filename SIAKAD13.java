f6a26c275635c49788fc1392d0a29e595bb20463
Nurwaely12 <nurwaely@gmail.com>
2024-09-24 19:55:54 +0700

    SIAKAD13

--- SIAKAD13.java
+++ SIAKAD13.java
@@ -1,6 +1,66 @@
+import java.util.Scanner;
+
 public class SIAKAD13 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+
         
+        System.out.print("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukkan kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukkan nomor absen: ");
+        absen = sc.nextByte();
+
+  
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan nilai ujian: ");
+        nilaiUjian = sc.nextDouble();
+
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+
+        
+        String nilaiHuruf = "";
+        String kualifikasi = "";
+
+        if (nilaiAkhir >= 80 && nilaiAkhir <= 100) {
+            nilaiHuruf = "A";
+            kualifikasi = "Sangat Baik";
+        } else if (nilaiAkhir > 73 && nilaiAkhir < 80) {
+            nilaiHuruf = "B+";
+            kualifikasi = "Lebih dari Baik";
+        } else if (nilaiAkhir >= 65 && nilaiAkhir <= 73) {
+            nilaiHuruf = "B";
+            kualifikasi = "Baik";
+        } else if (nilaiAkhir >= 60 && nilaiAkhir < 65) {
+            nilaiHuruf = "C+";
+            kualifikasi = "Lebih dari cukup";
+        } else if (nilaiAkhir >= 50 && nilaiAkhir < 60) {
+            nilaiHuruf = "C";
+            kualifikasi = "Cukup";
+        } else if (nilaiAkhir >= 39 && nilaiAkhir < 50) {
+            nilaiHuruf = "D";
+            kualifikasi = "Kurang";
+        } else if (nilaiAkhir >= 0 && nilaiAkhir < 39) {
+            nilaiHuruf = "E";
+            kualifikasi = "Gagal";
+        } else {
+            System.out.println("Nilai tidak valid.");
+            return;
+        }
+
+        System.out.println("\nMahasiswa dengan nama " + nama + " (NIM " + nim + ") kelas " + kelas + " nomor absen " + absen);
+        System.out.println("Nilai akhir: " + nilaiAkhir);
+        System.out.println("Nilai akhir huruf: " + nilaiHuruf);
+        System.out.println("Kualifikasi: " + kualifikasi);
     }
-    
 }
