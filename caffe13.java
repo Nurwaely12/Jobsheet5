a0a1e867b3ef20de83993e68730e27ec1008747e
Nurwaely12 <nurwaely@gmail.com>
2024-09-24 20:46:36 +0700

    caffe13

--- caffe13.java
+++ caffe13.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class Kafe13 {
+public class caffe13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); 
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        double totalHarga, nominalBayar; // Deklarasi variabel totalHarga dan nominalBayar
-        float diskon = 0.1f; // Diskon 10% untuk member
+        double totalHarga, nominalBayar; 
+        float diskon = 0.1f; 
 
-        // Input keanggotaan dan jumlah item yang dibeli
+       
         System.out.print("Apakah Anda member kafe? (true/false): ");
         keanggotaan = input.nextBoolean();
         System.out.print("Masukkan jumlah pembelian kopi: ");
@@ -19,7 +19,7 @@ public class Kafe13 {
         System.out.print("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
 
-        // Hitung total harga tanpa diskon
+      
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
 
         
@@ -29,9 +29,10 @@ public class Kafe13 {
             nominalBayar = totalHarga; 
         }
 
-        
+       
         System.out.println("\nKeanggotaan pelanggan: " + keanggotaan);
         System.out.println("Item pembelian: " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti ");
         System.out.println("Nominal bayar: Rp " + nominalBayar);
     }
 }
+
